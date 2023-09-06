ce4331a4129e5874d42db4fa94f2986abac92beb
minju kim <62362753+alswn2348@users.noreply.github.com>
2023-09-06 12:13:09 +0900

    chore : db 연결

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,23 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [ConfigModule.forRoot({
     isGlobal: true,
     envFilePath: `.env.${process.env.NODE_ENV}`
+  }
+  ),
+  TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: 'database-1.cwxpk5ykulue.ap-northeast-2.rds.amazonaws.com',
+    port: 5432,
+    username: 'postgres',
+    password: 'wn980622',
+    database: "postgres",
+    synchronize: true,
+    autoLoadEntities: true,
   })],
   controllers: [AppController],
   providers: [AppService],
